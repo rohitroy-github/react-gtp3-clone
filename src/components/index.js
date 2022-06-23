4629a1cff8a32c2e7beaab4916e1bf9a3eb2e668
rohit <rohitdevops.roy@gmail.com>
2022-06-23 14:14:33 +0530

    File Structure

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+// A common export file for all the functional components in in ./components
+
+export { default as Article } from './article/Article'; 
+export { default as Brand } from './brand/Brand'; 
+export { default as CTA } from './cta/CTA'; 
+export { default as Feature } from './feature/Feature'; 
+export { default as Navbar } from './navbar/Navbar';  
---
+++ src/containers/whatGPT3/WhatGp3.jsx
@@ -0,0 +1,10 @@
+import React from 'react'; 
+import 'WhatGp3.css';
+
+const WhatGp3 = () => {
+  return (
+    <div>WhatGp3</div>
+  )
+}
+
+export default WhatGp3;
