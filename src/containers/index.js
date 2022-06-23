5310d7d93a1381e5fda8894e888eaab673381c6a
rohit <rohitdevops.roy@gmail.com>
2022-06-23 14:21:16 +0530

    File Structure

---
+++ src/containers/index.js
@@ -0,0 +1,9 @@
+// A common export file for all the functional components in in ./containers
+
+export { default as Blog } from './blog/Blog'; 
+export { default as Features } from './features/Features'; 
+export { default as Footer } from './features/Features'; 
+export { default as Header } from './header/Header'; 
+export { default as WhatGP3 } from './whatGPT3/WhatGp3'; 
+export { default as Possibility } from './possibility/Possibility'; 
+
